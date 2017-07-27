cc00e686dbe0481c9817e78cfa14ab7df5ce4bfe
Henrique Dias <hacdias@gmail.com>
2017-07-27 17:21:07 +0100

    updates

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-'use strict';
+'use strict'
 
-module.exports = function (zipcode, country) {
-	if (typeof zipcode !== 'string') {
-		throw new TypeError('Expected a string for zipcode');
-	}
+module.exports = function (zipcode, country = 'US') {
+  if (typeof zipcode !== 'string') {
+    throw new TypeError('Expected a string for zipcode')
+  }
 
-    if (typeof country === 'undefined') {
-        country = 'US';
-    }
+  var zipcodes = require('zipcodes-regex')
 
-    var zipcodes = require('zipcodes-regex');
+  if (!zipcodes[country]) {
+    throw new ReferenceError('Invalid country ' + country)
+  }
 
-    if (!zipcodes[country]) {
-        throw new ReferenceError('Invalid country ' + country);
-    }
+  if (zipcodes[country] === 0) {
+    throw new TypeError('There is no regular expression for the country you selected');
+  }
 
-    if (zipcodes[country] === 0) {
-        throw new TypeError('There is no regular expression for the country you selected');
-    }
-
-    var regex = new RegExp(zipcodes[country]);
-    return regex.test(zipcode);
-};
+  var regex = new RegExp(zipcodes[country])
+  return regex.test(zipcode)
+}
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-'use strict';
-var assert = require('assert');
-var isValidZipcode = require('./');
+'use strict'
 
-assert(isValidZipcode('0000', 'AF'));
-assert(isValidZipcode('0000-000', 'PT'));
+var assert = require('assert')
+var isValidZipcode = require('./')
+
+assert(isValidZipcode('0000', 'AF'))
+assert(isValidZipcode('0000-000', 'PT'))
